3302ff77b94fc63265adbb944eaadd8d0a641688
Ikraam <ikraamghoor@gmail.com>
2020-07-24 08:47:30 +0200

    Added scripts to config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    './src/index.js',
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
   },
   module: {
     rules: [
